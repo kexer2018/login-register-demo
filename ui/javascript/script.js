09ecf68eaf24d936a7060e445f346c9057d96687
ZX <13164625109@163.com>
2023-06-01 23:59:36 +0800

    feat:add axios

--- ui/javascript/script.js
+++ ui/javascript/script.js
@@ -2,7 +2,7 @@
  *@author: Zhang Xiao
  *@date: 2023-06-01 14:01:07
  *@version: v1.0.0
-**/
+ **/
 
 /* 获取页面的文件 */
 const admin = document.querySelector('#admin')
@@ -11,9 +11,35 @@ const check = document.querySelector('#check')
 const btn = document.querySelector('#btn')
 
 /* 获取页面的数据 */
+function getAccount () {
+  return admin.value
+}
+function getPassword () {
+  return password.value
+}
 
 /* 登录按钮绑定事件 */
+btn.addEventListener('click', () => {
+  let account = getAccount().trim()
+  let password = getPassword().trim()
+  if (!account || !password) {
+    alert('请正确输入')
+  }
+  //利用正则表达式判断输入是否正确
+
+  //将页面的数据发送到到后端去
+  axios
+    .post('/userInfo', {
+      account: account,
+      password: password,
+      login_time: Date.now()
+    })
+    .then(function (response) {
+      console.log(response)
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+})
 
 /* axios发送请求后端 */
-
-
