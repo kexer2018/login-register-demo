ec0aeafc187e3fe4b75f826786b92fd7dca6b761
zhangxiao <zhangxiao@acoinfo.com>
2023-06-01 15:51:11 +0800

    feat:add front-end

---
+++ ui/javascript/script.js
@@ -0,0 +1,19 @@
+/**
+ *@author: Zhang Xiao
+ *@date: 2023-06-01 14:01:07
+ *@version: v1.0.0
+**/
+
+/* 获取页面的文件 */
+const admin = document.querySelector('#admin')
+const password = document.querySelector('#password')
+const check = document.querySelector('#check')
+const btn = document.querySelector('#btn')
+
+/* 获取页面的数据 */
+
+/* 登录按钮绑定事件 */
+
+/* axios发送请求后端 */
+
+
